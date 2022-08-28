2455e835fa3d61468973192a7aaa772b4a063e04
Joy <biwott.joy.03@gmail.com>
2022-08-28 18:00:38 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    form.reset();
+  })
 });
+
+function buildToDo(todo){
+  const li = document.createElement('li')
+  const list = document.querySelector('#toDo')
+  const btn = document.createElement('button')
+  const ul = document.querySelector('#tasks')
+  ul.appendChild(li);
+  li.textContent = `${todo} `
+  li.appendChild(btn);
+  btn.textContent = 'del'
+  btn.addEventListener('click', deleteAct);
+  
+}
+
+function deleteAct(e){
+  e.target.parentNode.remove();
+}
+
+
+  
+
